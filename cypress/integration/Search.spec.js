eeb950e5ca3a4ce75c9a5bdbd0cb913db92c0df2
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-11-18 18:18:49 -0300

    changin to localhost

--- cypress/integration/Search.spec.js
+++ cypress/integration/Search.spec.js
@@ -1,6 +1,6 @@
 context("Search CEP", () => {
     it("Enters the page and search the CEP", () => {
-        cy.visit('https://react-entrega-s5-criando-testes-larissakolivei-larissakoliveira.vercel.app')
+        cy.visit('http://localhost:3000/')
         cy.viewport(1440, 900)
         cy.get("input").type('22041001')
         cy.contains("Buscar pelo CEP").click();
