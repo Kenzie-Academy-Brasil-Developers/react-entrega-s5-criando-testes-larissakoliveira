602f2b126d05aa9baece1ea3374e02a6d58562cd
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-11-19 21:54:09 -0300

    fixing

--- cypress/integration/Search.spec.js
+++ cypress/integration/Search.spec.js
@@ -1,9 +1,30 @@
-context("Search CEP", () => {
-    it("Enters the page and search the CEP", () => {
+context("Enters the page and Search CEP", () => {
+    it("Enters the page", () => {
         cy.visit('http://localhost:3000/')
         cy.viewport(1440, 900)
-        cy.get("input").type('22041001')
-        cy.contains("Buscar pelo CEP").click();
+    });
 
-    })
-})
+    it("Search CEP", () => {
+        cy.viewport(1440, 900)
+
+        cy.intercept("GET", `${22041001}`, {
+            bairro: "Copacabana",
+      cep: 22041001,
+      cidade: "Rio de Janeiro",
+      cidade_info: { area_km2: "1200,179", codigo_ibge: "3304557" },
+      complemento: "de 2174 a 2634 - lado par",
+      estado: "RJ",
+      estado_info: {
+        area_km2: "43.781,566",
+        codigo_ibge: "33",
+        nome: "Rio de Janeiro",
+      },
+      logradouro: "Avenida Atlântica",
+        });
+        
+            cy.get("input").type(22041001)
+            cy.contains("Buscar pelo CEP").click();
+    });
+
+
+});
